1642a1a0ddd3f35ac8e1b501c7e8474acdd8db2c
Rahma Nurhaliza <111843281+RahmaNhz@users.noreply.github.com>
2025-08-08 02:59:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, redirect, url_for
+from model_loader import load_model_and_search
+from model_labels import MODEL_LABELS
+import json
+import os
+
+app = Flask(__name__)
+
+RELEVANSI_FILE = 'data/relevansi.json'
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    results = []
+    expanded_query = []
+    query = ""
+    mode = ""
+    selected_model_label = ""
+
+    if request.method == "POST":
+        query = request.form["query"]
+        model_name = request.form["model"]
+        mode = request.form["mode"]
+
+        search_result = load_model_and_search(query, model_name, mode)
+        expanded_query = search_result["expanded_query"]
+        results = search_result["results"]
+
+        selected_model_label = MODEL_LABELS.get(model_name, model_name)
+
+    return render_template("index.html", results=results, expanded_query=expanded_query, query=query, mode=mode, selected_model_label=selected_model_label)
+
+@app.route("/simpan_relevansi", methods=["POST"])
+def simpan_relevansi():
+    query = request.form.get("query")
+    model = request.form.get("model")
+    data = request.form.to_dict()
+
+    relevan = []
+    tidak_relevan = []
+
+    for k, v in data.items():
+        if k.startswith("relevansi_"):
+            doc_id = k.split("_")[1]
+            if v == "relevan":
+                relevan.append(doc_id)
+            elif v == "tidak":
+                tidak_relevan.append(doc_id)
+
+    simpanan = {
+        "query": query,
+        "model": model,
+        "dokumen_relevan": relevan,
+        "dokumen_tidak_relevan": tidak_relevan,
+        "total_relevan": len(relevan),
+        "total_tidak_relevan": len(tidak_relevan)
+    }
+
+    # Load file lama
+    if os.path.exists(RELEVANSI_FILE):
+        with open(RELEVANSI_FILE, "r") as f:
+            all_data = json.load(f)
+    else:
+        all_data = []
+
+    all_data.append(simpanan)
+
+    with open(RELEVANSI_FILE, "w") as f:
+        json.dump(all_data, f, indent=2)
+
+    return redirect(url_for("lihat_relevansi"))
+
+# Placeholder route untuk melihat hasil
+@app.route("/lihat_relevansi")
+def lihat_relevansi():
+    if os.path.exists(RELEVANSI_FILE):
+        with open(RELEVANSI_FILE, "r") as f:
+            data = json.load(f)
+    else:
+        data = []
+
+    return render_template("relevansi.html", data=data)
+
+#------Route untuk menghapus----#
+@app.route("/relevansi/hapus/<int:index>")
+def hapus_relevansi(index):
+    # Misalnya kamu simpan relevansi di file JSON
+    with open("data/relevansi.json", "r") as f:
+        data = json.load(f)
+
+    if 0 <= index < len(data):
+        data.pop(index)
+
+        with open("data/relevansi.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+    return redirect(url_for("lihat_relevansi"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
---
+++ model_loader.py
@@ -0,0 +1,103 @@
+import re #untuk tokenisasi (memecah teks ke kata)
+import numpy as np 
+import pandas as pd
+import ast
+from gensim.models import Word2Vec
+from nltk.corpus import stopwords
+from rank_bm25 import BM25Plus
+
+stop_words = set(stopwords.words('indonesian'))
+
+#preprocessing
+def preprocess_text(text, use_str=True):
+    text = text.lower()
+    tokens = re.findall(r'\b\w+\b', text)
+    if use_str:
+        tokens = [t for t in tokens if t not in stop_words]  # hanya hapus stopword jika STR
+    return tokens
+
+#Fungsi Expanded Query
+#model = model Word2Vec yang sudah dilatih
+def expand_query(model, query_tokens, topn=5): 
+    valid_tokens = [w for w in query_tokens if w in model.wv]
+    if not valid_tokens:
+        return query_tokens
+    if len(valid_tokens) == 1:
+        similar = model.wv.most_similar(valid_tokens[0], topn=topn*3)
+    else:
+        vectors = [model.wv[w] for w in valid_tokens]
+        avg_vector = np.mean(vectors, axis=0)
+        similar = model.wv.similar_by_vector(avg_vector, topn=topn*3)
+
+    added = []
+    for w, _ in similar:
+        if w not in valid_tokens and w not in added:
+        #if w not in valid_tokens and w not in stop_words and w not in added:
+            added.append(w)
+        if len(added) >= topn:
+            break
+
+    return valid_tokens + added
+
+#PENCARIAN DOKUMEN BM25+
+#model_name: nama model Word2Vec yang akan diload
+#mode = mode pencarian (QE tanpa Stopword Removal, QE dengan Stopword Removal, Tanpa QE)
+def load_model_and_search(query, model_name, mode):
+    print(f"\n=== LOG: Sedang load model: {model_name} ===")
+    print(f">>> Mode pencarian: {mode}")
+    
+    use_qe = mode in ["QE_STR", "QE_NOSTR"]
+    use_str = mode == "QE_STR" or mode == "NO_QE"
+
+    # Load corpus yang telah dipreprocessing dengan menggunakan Stopword Removal atau tidak
+    if use_str:
+        print(">>> Memakai CORPUS STR")
+        df = pd.read_csv('data/preprocessing.csv')
+        df['berita_preprocessed'] = df['berita_preprocessed'].apply(ast.literal_eval)
+        corpus_tokenized = df['berita_preprocessed'].tolist()
+    else:
+        print(">>> Memakai CORPUS NON STR")
+        df = pd.read_csv('data/preprocessingnostr.csv')
+        df['preprocesing-nostopremov'] = df['preprocesing-nostopremov'].apply(ast.literal_eval)
+        corpus_tokenized = df['preprocesing-nostopremov'].tolist()
+
+
+    #corpus_tokenized = [preprocess_text(doc, use_str) for doc in corpus]
+#Inisialisasi BM25+
+    bm25 = BM25Plus(corpus_tokenized, k1=1.2, b=0.75, delta=1.0)
+    #preprocessing query
+    query_tokens = preprocess_text(query, use_str)
+    print(f">>> Query awal: {query_tokens}")
+
+    
+    if use_qe: #jika menggunakan stopwordremoval (use_str)
+        model_path = f'models/{model_name}.model'
+        print(f">>> Load model dari: {model_path}")
+        model = Word2Vec.load(model_path)
+        expanded_query = expand_query(model, query_tokens)
+        print(f">>> Query diperluas: {expanded_query}")
+    else:
+        expanded_query = query_tokens
+        print(">>> Tanpa query expansion.")
+    #Hitung skor BM25+
+    scores = bm25.get_scores(expanded_query) #Menghitung skor BM25+ untuk semua dokumen terhadap query (atau query yang diperluas).
+    top_n = 10
+    top_indices = np.argsort(scores)[::-1][:top_n]
+
+    results = []
+    for idx in top_indices:
+        row = df.iloc[idx]
+        results.append({
+            'No': row['No'],
+            'Judul': row['judul'],
+            'Berita': row['berita'],
+            'Tanggal': row['tanggal'],
+            'Kategori': row['kategori'],
+            'Link': row['link'],
+            'Skor': round(scores[idx], 4)
+        })
+
+    return {
+        "expanded_query": expanded_query,
+        "results": results,
+    }
